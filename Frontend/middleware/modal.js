62a46d8e9e1d7602025808848ddba962c7f7374d
Malony Jaramillo <malony.jaramillo@utp.ac.pa>
2023-08-10 10:55:51 -0500

    se arregla el evento de la tarea flotante

--- Frontend/middleware/modal.js
+++ Frontend/middleware/modal.js
@@ -1,23 +1,45 @@
-const Modal=document.getElementById("Modal")
+const dialogo=document.getElementById("dialogo")
 
-let tareas= document.getElementsByClassName("Tareas")
 
-console.log(tareas[0])
+// Obtén todas las tareas con la clase 'Tareas'
+const tareas = document.querySelectorAll(".Tareas");
 
-for(let i in tareas.length){
-    tareas[i].addEventListener("click",()=>{
-        Modal.showModal()
-    })
-}
+// Agrega el evento de clic a todas las tareas
+tareas.forEach(tarea => {
+    tarea.addEventListener("click", () => {
+        dialogo.showModal();
+    });
+});
 
-Modal.addEventListener("click", e => {
-    const dialogDimensions = Modal.getBoundingClientRect()
+dialogo.addEventListener("click", e => {
+    const dialogDimensions = dialogo.getBoundingClientRect();
     if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        dialogo.close();
+    }
+});
+
+
+
+/* El siguiente codigo funciona
+document.getElementById("Open_Dialog").addEventListener("click",()=>{
+    dialogo.showModal()
+})
+
+dialogo.addEventListener("click", e => {
+    const dialogDimensions = dialogo.getBoundingClientRect()
+    if(
       e.clientX < dialogDimensions.left ||
       e.clientX > dialogDimensions.right ||
       e.clientY < dialogDimensions.top ||
       e.clientY > dialogDimensions.bottom
-    ) {
-        Modal.close()
+    ){
+      dialogo.close()
     }
   })
+
+  */
