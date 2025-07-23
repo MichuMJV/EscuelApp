e737c0e2e21a47fd2de71e4c5dc0fa12ff31cad7
Malony <malony.jaramillo@utp.ac.pa>
2025-07-23 09:05:53 -0500

    ahora si funciona gracias a gemini

--- index.js
+++ index.js
@@ -1,76 +1,95 @@
-const express= require("express")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path"); // Import the 'path' module
 
-const app=express()
+const basededatos = require("./config/db.js");
+const port = 5000;
 
-const cors=require("cors")
+// --- Database Connection ---
+basededatos();
 
-const basededatos= require("./config/db.js")
+// --- Controller Imports ---
+const GetSalonDetails = require("./controllers/GetSalonDetails.js");
+const DeleteUser = require("./controllers/DeleteUser.js");
+const DeleteSalon = require("./controllers/DeleteSalon.js");
+const DeleteApp = require("./controllers/DeleteApp.js");
+const EstudianteEntregaTarea = require("./controllers/EstudianteEntregaTarea.js");
+const ReturnApps = require("./controllers/ReturnApps.js");
+const Matricular = require("./controllers/Matricular.js");
+const modificarMatricula = require("./controllers/modificarMatricula.js");
+const NewApp = require("./controllers/NewApp.js");
+const UpdateApp = require("./controllers/UpdateApp.js");
+const NewSalon = require("./controllers/NewSalon.js");
+const NewTarea = require("./controllers/NewTarea.js");
+const register = require("./controllers/register.js");
+const NotaTarea = require("./controllers/NotaTarea.js");
+const returnMatricula = require("./controllers/returnMatricula.js");
+const returnStudentClasses = require("./controllers/returnStudentClasses.js");
+const returnTeachersSalon = require("./controllers/returnTeachersSalon.js");
+const returnUser = require("./controllers/returnUser.js");
+const ReturnAllSalons = require("./controllers/ReturnAllSalons.js");
+const ReturnIDCodMateria = require("./controllers/ReturnIDMateria.js");
+const ReturnSalonsByCodSalon = require("./controllers/ReturnSalonsByCodSalon.js");
+const ReturnSalonsByGrado = require("./controllers/ReturnSalonsByGrado.js");
+const ReturnSalonsByProfessor = require("./controllers/ReturnSalonsByProfessor.js");
+const startSesion = require("./controllers/startSesion.js");
+const NuevoRol = require("./controllers/UpdateRol.js");
+const UpdateUsers = require("./controllers/UpdateUsers.js");
+const UpdateSalon = require("./controllers/UpdateSalon.js");
 
-const port=5000
+// --- Middleware ---
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-basededatos()
+// --- FIX: Serve Static Files ---
+// This line tells Express to serve any files from the 'Frontend' directory.
+// The 'path.join' is used to create a reliable path regardless of the operating system.
+// This assumes your 'Frontend' folder is in the same directory as this server file.
+app.use(express.static(path.join(__dirname, 'Frontend')));
 
-const GetSalonDetails=require("./controllers/GetSalonDetails.js")
-const DeleteUser=require("./controllers/DeleteUser.js")
-const DeleteSalon=require("./controllers/DeleteSalon.js")
-const DeleteApp=require("./controllers/DeleteApp.js")
-const EstudianteEntregaTarea=require("./controllers/EstudianteEntregaTarea.js")
-const ReturnApps=require("./controllers/ReturnApps.js")
-const Matricular=require("./controllers/Matricular.js")
-const modificarMatricula=require("./controllers/modificarMatricula.js")
-const NewApp=require("./controllers/NewApp.js")
-const UpdateApp=require("./controllers/UpdateApp.js")
-const NewSalon=require("./controllers/NewSalon.js")
-const NewTarea=require("./controllers/NewTarea.js")
-const register=require("./controllers/register.js")
-const NotaTarea=require("./controllers/NotaTarea.js")
-const returnMatricula=require("./controllers/returnMatricula.js")
-const returnStudentClasses=require("./controllers/returnStudentClasses.js")
-const returnTeachersSalon=require("./controllers/returnTeachersSalon.js")
-const returnUser=require("./controllers/returnUser.js")
-const ReturnAllSalons=require("./controllers/ReturnAllSalons.js")
-const ReturnIDCodMateria=require("./controllers/ReturnIDMateria.js")
-const ReturnSalonsByCodSalon=require("./controllers/ReturnSalonsByCodSalon.js")
-const ReturnSalonsByGrado=require("./controllers/ReturnSalonsByGrado.js")
-const ReturnSalonsByProfessor=require("./controllers/ReturnSalonsByProfessor.js")
-const startSesion=require("./controllers/startSesion.js")
-const NuevoRol=require("./controllers/UpdateRol.js")
-const UpdateUsers = require("./controllers/UpdateUsers.js")
-const UpdateSalon = require("./controllers/UpdateSalon.js")
 
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
+// --- API Routes ---
 
-app.delete("/Escuelapp/DeleteSalon",DeleteSalon)
-app.delete("/Escuelapp/DeleteApp",DeleteApp)
-app.delete("/Escuelapp/DeleteUser",DeleteUser)
+// DELETE Routes
+app.delete("/Escuelapp/DeleteSalon", DeleteSalon);
+app.delete("/Escuelapp/DeleteApp", DeleteApp);
+app.delete("/Escuelapp/DeleteUser", DeleteUser);
 
-app.put("/Escuelapp/modificarMatricula",modificarMatricula)
-app.put("/Escuelapp/NotaTarea",NotaTarea)
-app.put("/Escuelapp/nuevoRol",NuevoRol)
-app.put("/Escuelapp/UpdateApp",UpdateApp)
-app.put("/Escuelapp/UpdateUsers",UpdateUsers)
-app.put("/Escuelapp/UpdateSalon",UpdateSalon)
+// PUT Routes
+app.put("/Escuelapp/modificarMatricula", modificarMatricula);
+app.put("/Escuelapp/NotaTarea", NotaTarea);
+app.put("/Escuelapp/nuevoRol", NuevoRol);
+app.put("/Escuelapp/UpdateApp", UpdateApp);
+app.put("/Escuelapp/UpdateUsers", UpdateUsers);
+app.put("/Escuelapp/UpdateSalon", UpdateSalon);
 
-app.post("/Escuelapp/inicio_sesion",startSesion)
-app.post("/Escuelapp/EstudianteEntregaTarea",EstudianteEntregaTarea)
-app.post("/Escuelapp/Matricular",Matricular)
-app.post("/Escuelapp/NewApp",NewApp)
-app.post("/Escuelapp/NewSalon",NewSalon)
-app.post("/Escuelapp/NewTarea",NewTarea)
-app.post("/Escuelapp/Register",register)
+// POST Routes
+app.post("/Escuelapp/inicio_sesion", startSesion);
+app.post("/Escuelapp/EstudianteEntregaTarea", EstudianteEntregaTarea);
+app.post("/Escuelapp/Matricular", Matricular);
+app.post("/Escuelapp/NewApp", NewApp);
+app.post("/Escuelapp/NewSalon", NewSalon);
+app.post("/Escuelapp/NewTarea", NewTarea);
+app.post("/Escuelapp/Register", register);
 
-app.get("/Escuelapp/GetSalonDetails",GetSalonDetails)
-app.get("/Escuelapp/ReturnApps",ReturnApps)
-app.get("/Escuelapp/AllSalones",ReturnAllSalons)
-app.get("/Escuelapp/returnEstudiantesDeGrupo",returnMatricula)
-app.get("/Escuelapp/Materias-de-estudiante",returnStudentClasses)
-app.get("/Escuelapp/returnTeachersSalon",returnTeachersSalon)
-app.get("/Escuelapp/returnUser",returnUser)
-app.get("/Escuelapp/ReturnIDCodMateria",ReturnIDCodMateria)
-app.get("/Escuelapp/ReturnSalonsByCodSalon",ReturnSalonsByCodSalon)
-app.get("/Escuelapp/ReturnSalonsByGrado",ReturnSalonsByGrado)
-app.get("/Escuelapp/ReturnSalonsByProfessor",ReturnSalonsByProfessor)
+// GET Routes
+app.get("/Escuelapp/GetSalonDetails", GetSalonDetails);
+app.get("/Escuelapp/ReturnApps", ReturnApps);
+app.get("/Escuelapp/AllSalones", ReturnAllSalons);
+app.get("/Escuelapp/returnEstudiantesDeGrupo", returnMatricula);
+app.get("/Escuelapp/Materias-de-estudiante", returnStudentClasses);
+app.get("/Escuelapp/returnTeachersSalon", returnTeachersSalon);
+app.get("/Escuelapp/returnUser", returnUser);
+app.get("/Escuelapp/ReturnIDCodMateria", ReturnIDCodMateria);
+app.get("/Escuelapp/ReturnSalonsByCodSalon", ReturnSalonsByCodSalon);
+app.get("/Escuelapp/ReturnSalonsByGrado", ReturnSalonsByGrado);
+app.get("/Escuelapp/ReturnSalonsByProfessor", ReturnSalonsByProfessor);
 
-app.listen(port, ()=> console.log(`exampl app listening on port ${port}! the link is  http://127.0.0.1:${port}/Frontend/screens/inicio_sesion.html`))
+
+// --- Start Server ---
+app.listen(port, () => {
+    // The URL is now corrected. Because 'Frontend' is the static root, you don't include it in the path.
+    console.log(`Server listening on port ${port}! The link is http://127.0.0.1:${port}/screens/inicio_sesion.html`);
+});
